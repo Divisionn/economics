6a09cebad4b4af0382bbfed367373e37c73979ae
Divisionn <118789722+Divisionn@users.noreply.github.com>
2024-03-16 14:34:35 -0400

    added test block

---
+++ src/main/java/net/mcreator/economics/init/EconomicsModTabs.java
@@ -0,0 +1,29 @@
+
+/*
+ *    MCreator note: This file will be REGENERATED on each build.
+ */
+package net.mcreator.economics.init;
+
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.core.registries.Registries;
+
+import net.mcreator.economics.EconomicsMod;
+
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+public class EconomicsModTabs {
+	public static final DeferredRegister<CreativeModeTab> REGISTRY = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, EconomicsMod.MODID);
+
+	@SubscribeEvent
+	public static void buildTabContentsVanilla(BuildCreativeModeTabContentsEvent tabData) {
+
+		if (tabData.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS) {
+			tabData.accept(EconomicsModBlocks.TEST_BLOCK.get().asItem());
+		}
+	}
+}
